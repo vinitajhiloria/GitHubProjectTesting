4411ca3e38d76abcd29893d0a38e0c7b54f0624b
Rubby <Rubby@DESKTOP-EVVLDNC>
2019-02-19 21:09:09 +0530

    Here is the master Project pushing comment.

---
+++ GitHubProjectTesting1/src/main/java/com/gitHubProjectTesting1/model/GitHubMember.java
@@ -0,0 +1,32 @@
+package com.gitHubProjectTesting1.model;
+
+public class GitHubMember {
+	private Long id;
+	private String mailId;
+	private String name;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMailId() {
+		return mailId;
+	}
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	private long phoneNumber;
+}
